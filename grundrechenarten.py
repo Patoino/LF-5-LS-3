7c69e824b83f303a3748bd5bd712fc01e228d98f
Patoino <90381194+Patoino@users.noreply.github.com>
2022-01-17 10:57:53 +0100

    Create grundrechenarten.py

---
+++ grundrechenarten.py
@@ -0,0 +1,27 @@
+"""
+
+"""
+#Addiert in1 mit in2
+def addieren(in1,in2):
+    ergebnis = in1 + in2
+    return ergebnis
+#Rechnet in1 minus in2
+def subtrahieren(in1,in2):
+    ergebnis = in1 - in2
+    return ergebnis
+#Multipliziert in1 mit in2
+def multiplizieren(in1,in2):
+    ergebnis = in1 * in2
+    return ergebnis
+#Teilt in1 durch in2
+def dividieren(in1,in2):
+    ergebnis = in1 / in2
+    return ergebnis
+#Berechnet die in2te Potenz von in1
+def potenz(in1,in2):
+    ergebnis = in1 ** in2
+    return ergebnis
+#Berechnet die 1. Wurzel
+def wurzel(in1):
+    ergebnis = in1 ** (0.5)
+    return ergebnis
