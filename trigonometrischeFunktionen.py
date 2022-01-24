7fb820d4eb350a49a1950a7cdec3b1b13bc42e5f
Patoino <90381194+Patoino@users.noreply.github.com>
2022-01-24 10:28:57 +0100

    Update trigonometrischeFunktionen.py

--- trigonometrischeFunktionen.py
+++ trigonometrischeFunktionen.py
@@ -1,1 +1,19 @@
-
+import math as m
+def sinus(li1):
+    ergebnis = m.sin(li1) #Ergebnis in rad und nicht in deg
+    return ergebnis
+def cosinus(li1):
+    ergebnis =m.cos(li1) #Ergebnis in rad und nicht in deg
+    return ergebnis
+def tangens(li1):
+    ergebnis = m.tan(li1) #Ergebnis in rad und nicht in deg
+    return ergebnis
+def aSin(li1):
+    ergebnis = m.asin(li1)
+    return ergebnis
+def aCos(li1):
+    ergebnis = m.acos(li1)
+    return ergebnis
+def aTan(li1):
+    ergebnis = m.atan(li1)
+    return ergebnis
